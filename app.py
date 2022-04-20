36a3f37a297f434a4d45b5010ccc36b4ca027821
Dev Ngecu <devngecu@gmail.com>
2022-04-20 06:40:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,67 @@
 # app.py
 import random
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify  
+import requests
+
+from requests.exceptions import HTTPError
+
+
+
+def get_api():
+
+    url = "https://get.geojs.io/"
+
+
+    try:
+        response = requests.get(url)
+
+    
+        response.raise_for_status()
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')  
+    except Exception as err:
+        print(f'Other error occurred: {err}') 
+    else:
+        print('Success! gotten your api')
+        x = (response.text).split()
+        ip_address = x[1]
+        print(ip_address)
+
+        return ip_address
+
+        # print(x)
+
+
+def get_latitude():
+    my_api = get_api()
+    url = "https://get.geojs.io/v1/ip/geo/{}.json".format(my_api)
+
+    try:
+        response = requests.get(url)
+
+    
+        response.raise_for_status()
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')  
+    except Exception as err:
+        print(f'Other error occurred: {err}') 
+    else:
+        print('Success! gotten your coordinates')
+        x = response.json()
+        print("Longitude ,",x["longitude"])
+        print("Latitude ,",x["latitude"])
+
+
+        return x
+        # x = (response.text).split()
+        # ip_address = x[1]
+        # print(ip_address)
 
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
-def respond():
-    data=[]
-    for n in range(11):
-        data.append({'id':n,'Raw_Sensor_Value':random.randint(500, 550),
-                      'Voltage_(mV)':random.randint(155, 389)/1000,            
-                       'Current_(A)':random.randint(-155, 389)/1000
-                    })
-    
-    # Return the response in json format
-    return jsonify(data)
+get_latitude()
 
 
 
@@ -27,3 +73,9 @@ def respond():
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000)
+
+
+
+
+
+
